e4dd16247d5aa8b4de0d898b3912fd5441b6016d
Eric Thiel <ethiel172@gmail.com>
2019-06-27 15:28:24 -0400

    fixed letter replacement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,16 +14,17 @@ var lossesText = document.getElementById("losses");
 
 // Possible words and choosing the goal word
 var possibleWords = ["a", "be", "cuf", "diey"];
-var wordDisplay = ""
+var wordDisplay = []
 var computerGuess = possibleWords[Math.floor(Math.random() * possibleWords.length)];
 // log win condition for debugging
 console.log("the word is " + computerGuess);
 
 // loop that adds "_" to a for computerGuess.length
 for (var i = 0; computerGuess.length > i; i++ ){
-  wordDisplay = wordDisplay +  "_";
+  wordDisplay.push("_")
 };
-goalWordText.textContent = wordDisplay;
+goalWordText.textContent = wordDisplay.join(" ");
+
 
 
 // Key press event Listener
@@ -36,9 +37,9 @@ document.onkeyup = function(event){
     // If key pressed is one of the letters in the word
     // replace the "_" with the guessed letter
     if (guess === computerGuess[i]){
+      console.log(true);
       wordDisplay[i] = guess;
-      console.log(wordDisplay);
-      goalWordText.textContent = wordDisplay;
+      goalWordText.textContent = wordDisplay.join(" ");
     }
     else{
       console.log(false)
