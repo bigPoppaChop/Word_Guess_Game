eb3508d2f8f493aebfe9ae35fecf8077a1980a44
Eric Thiel <ethiel172@gmail.com>
2019-06-27 16:36:39 -0400

    basic game functionality complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,10 +12,17 @@ var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 
 
-// Possible words and choosing the goal word
-var possibleWords = ["a", "be", "cuf", "diey"];
+// Game Data
+var possibleWords = ["alchemist", "abbadon", "slark", "viper"];
+var chances = 10;
+var wins = 0;
+var losses = 0;
 var wordDisplay = []
+var lettersGuessed = [];
 var computerGuess = possibleWords[Math.floor(Math.random() * possibleWords.length)];
+//add array of alphabet to check key pressed is a letter
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // log win condition for debugging
 console.log("the word is " + computerGuess);
 
@@ -30,19 +37,58 @@ goalWordText.textContent = wordDisplay.join(" ");
 // Key press event Listener
 document.onkeyup = function(event){
   var guess =  event.key.toLocaleLowerCase();  
-  console.log("User Guess: " + guess);
-  // when key is pressed loop through each character of computerGuess   
-  for (i = 0; computerGuess.length > i; i++){
+  //If pressed key is not in alphabet array
+  if (alphabet.indexOf(guess)=== -1){
+    alert("Only guess letters please.");
+  }
+  // If letter was already guessed
+  else if (lettersGuessed.indexOf(guess) > -1 ){
+    alert("You've already guessed that key");
+  }
 
+  // If letter is NOT in the word
+  else if (computerGuess.indexOf(guess) === -1){
+    // Decrease number of chances
+    chances = chances - 1;
+    // Add letter guessed to lettersGuessed array
+    lettersGuessed.push(guess);
+    lettersGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
+
+    // Update rest of page text
+    userChoiceText.textContent = "Your Guess: " + guess;
+    chancesText.textContent = "Remaining Guesses: " + chances;
+    
+    // If the player runs out of guesses
+    if (chances === 0){
+      losses++;
+      winsText.textContent = "Wins: " + wins;
+      lossesText.textContent = "Losses: " + losses;
+
+      alert("You lose, refresh to try again")
+    }
+  }
+
+  // If letter IS in the word
+  else if (computerGuess.indexOf(guess) > -1){
+
+    // Update page
+    userChoiceText.textContent = "Your Guess: " + guess;
+    lettersGuessed.push(guess);
+    lettersGuessedText.textContent = "Letters Guessed: " + lettersGuessed;
+     
+    for (i = 0; computerGuess.length > i; i++){
     // If key pressed is one of the letters in the word
     // replace the "_" with the guessed letter
-    if (guess === computerGuess[i]){
-      console.log(true);
-      wordDisplay[i] = guess;
-      goalWordText.textContent = wordDisplay.join(" ");
-    }
-    else{
-      console.log(false)
+      if (guess === computerGuess[i]){
+        wordDisplay[i] = computerGuess[i];
+        goalWordText.textContent = wordDisplay.join(" ");
+        if (wordDisplay.indexOf("_") === -1){
+          alert("you win");
+          wins++;
+          winsText.textContent = "Wins: " + wins;
+          lossesText.textContent = "Losses: " + losses;
+        }
+      }
     }
   }
 }
